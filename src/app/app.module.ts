1152bd5b0e37c99940440ebcf5dbfba4d77ad438
Nataly <natalycarvalho.dev@gmail.com>
2022-09-01 16:56:23 -0300

    Cadastro e entrar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroComponent,
+    EntrarComponent
   ],
   imports: [
     BrowserModule
