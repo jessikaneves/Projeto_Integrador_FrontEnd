7180063e3a7d12e9b8e4dfec7eb8fdc97b542062
vicafz <victoriapfarias@gmail.com>
2022-09-08 17:33:59 -0300

    commit login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 const routes: Routes = [
 
@@ -10,6 +11,8 @@ const routes: Routes = [
   
     {path: 'entrar', component: EntrarComponent},
     {path: 'cadastrar', component: CadastroComponent},
+
+    {path:'inicio', component: InicioComponent}
     
   
     
