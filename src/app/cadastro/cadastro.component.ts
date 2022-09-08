730e2dc32453847e93aac99784e2ffdc0dd13dfa
Aline Beltrao <alinebelltrao@gmail.com>
2022-09-08 16:17:50 -0300

    api de cadastro

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
+
 
 @Component({
   selector: 'app-cadastro',
@@ -7,9 +11,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor() { }
+  user: User = new User
 
-  ngOnInit(): void {
+  confirmarSenha: string
+  tipoUsuario: string
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0,0)
   }
+
+  confirmSenha(event: any) {
+    this.confirmarSenha = event.target.value
+  }
+
+  tipoUser(event: any) {
+    this.tipoUsuario = event.target.value
+  }
+
+  cadastrar() {
+    this.user.tipo = this.tipoUsuario
+
+    if(this.user.senha != this.confirmarSenha) {
+      alert("As senhas estão diferentes!")
+    } else this.authService.cadastrar(this.user).subscribe((resp: User) => {
+      this.user = resp
+      alert("Usuário cadastrado com sucesso!")
+      this.router.navigate(['/login'])
+    })
+
+
+  }
+
+
 
 }
